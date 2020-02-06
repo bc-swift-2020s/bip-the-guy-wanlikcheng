c5a45f38075431f8632abccb28029d30f206c763
kelvin <chengwj@bc.edu>
2020-02-06 14:33:35 -0500

    Completed - 4.3 Adding Sound by Reusing Our PlaySound Function

--- Bip the Guy/ViewController.swift
+++ Bip the Guy/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     // properties
     @IBOutlet weak var imageToPunch: UIImageView!
+    
+    var audioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +31,25 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
     }
     
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName)
+        {
+            do
+            {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            }
+            catch
+            {
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        }
+        else
+        {
+            print("ERROR: Could not read data from sound file.")
+        }
+    }
+    
     // actions
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
@@ -37,6 +59,7 @@ class ViewController: UIViewController {
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
         animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
     }
 }
 
